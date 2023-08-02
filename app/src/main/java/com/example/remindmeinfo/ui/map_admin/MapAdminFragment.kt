478aee627e72971ad63b8f6d6be6f4ea2b046afa
AndyNuzzi <andy.nuzzi@gmail.com>
2023-08-02 12:09:38 +0200

    correcting mistakes from renaming files for admin interfaces

--- app/src/main/java/com/example/remindmeinfo/ui/map_admin/MapAdminFragment.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/map_admin/MapAdminFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.remindmeinfo.databinding.FragmentMapBinding
+import com.example.remindmeinfo.databinding.FragmentMapAdminBinding
 
 class MapAdminFragment : Fragment() {
 
-    private var _binding: FragmentMapBinding? = null
+    private var _binding: FragmentMapAdminBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class MapAdminFragment : Fragment() {
         val mapViewModel =
             ViewModelProvider(this).get(MapAdminViewModel::class.java)
 
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        _binding = FragmentMapAdminBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textMap
