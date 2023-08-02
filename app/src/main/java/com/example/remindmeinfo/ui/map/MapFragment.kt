aad84896b0d8e3a74a016115c7fe7caac5aa9743
AndyNuzzi <andy.nuzzi@gmail.com>
2023-08-02 11:44:31 +0200

    adding map fragment and model view

--- app/src/main/java/com/example/remindmeinfo/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/map/MapFragment.kt
@@ -7,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.remindmeinfo.databinding.FragmentMapBinding
 import com.example.remindmeinfo.databinding.FragmentNotificationsBinding
 
 class MapFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentMapBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +23,14 @@ class MapFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
+        val mapViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textMap
+        mapViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
