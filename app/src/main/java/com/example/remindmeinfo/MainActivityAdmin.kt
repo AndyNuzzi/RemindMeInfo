25bac66850a00fd7fd1004c7785b198a49c150ef
AndyNuzzi <andy.nuzzi@gmail.com>
2023-08-03 13:34:19 +0200

    adding menu for help and adjustments

--- app/src/main/java/com/example/remindmeinfo/MainActivityAdmin.kt
+++ app/src/main/java/com/example/remindmeinfo/MainActivityAdmin.kt
@@ -31,7 +31,7 @@ class MainActivityAdmin : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 //R.id.navigation_home, R.id.navigation_notifications,
-                R.id.navigation_dashboard, R.id.navigation_map
+                R.id.navigation_reminder, R.id.navigation_map
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/remindmeinfo/ui/reminder_admin/ReminderAdminFragment.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/reminder_admin/ReminderAdminFragment.kt
@@ -28,7 +28,7 @@ class ReminderAdminFragment : Fragment() {
         _binding = FragmentReminderAdminBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
+        val textView: TextView = binding.textReminder
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
