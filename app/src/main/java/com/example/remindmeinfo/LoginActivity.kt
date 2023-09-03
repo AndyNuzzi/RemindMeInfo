208bc81e966f13db6a49e394ee85ad57242b429d
AndyNuzzi <andy.nuzzi@gmail.com>
2023-09-03 19:44:11 +0200

    adding sections to the nav menu

--- app/src/main/java/com/example/remindmeinfo/LoginActivity.kt
+++ app/src/main/java/com/example/remindmeinfo/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.remindmeinfo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //val intent = Intent(this, MainActivityAdmin::class.java)
+        //startActivity(intent)
     }
 }
