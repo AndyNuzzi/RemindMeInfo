202941533f711728b808ded58c1db21b9a3423b8
AndyNuzzi <andy.nuzzi@gmail.com>
2023-09-09 14:32:07 +0200

    storing information into the ddbb

--- app/src/main/java/com/example/remindmeinfo/RegisterActivity.kt
+++ app/src/main/java/com/example/remindmeinfo/RegisterActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -23,22 +24,33 @@ class RegisterActivity : AppCompatActivity() {
     fun saveRegister(view: View){
         var email = currentUser?.email.toString()
         var dbRegister = FirebaseFirestore.getInstance()
+
+        var name = findViewById<EditText>(R.id.textName).getText().toString()
+
+        var surname = findViewById<EditText>(R.id.textSurname).getText().toString()
+        var age = findViewById<EditText>(R.id.textAge).getText().toString()
+        var genre = findViewById<EditText>(R.id.textGenre).getText().toString()
+        var info = findViewById<EditText>(R.id.textMedicalInfo).getText().toString()
+        var familyName = findViewById<EditText>(R.id.familyName).getText().toString()
+
+        var check = findViewById<CheckBox>(R.id.check_admin_app)
+        var check_verified = check.isChecked
+
+
         dbRegister.collection("users").document(email).update(
-            "bool_admin", false)
+            "bool_admin", check_verified)
         dbRegister.collection("users").document(email).update(
-            "name", "")
+            "name", name)
         dbRegister.collection("users").document(email).update(
-            "first_surname", "")
+            "surname", surname)
         dbRegister.collection("users").document(email).update(
-            "second_surname", "")
+            "age", age)
         dbRegister.collection("users").document(email).update(
-            "age", 0)
+            "genre", genre)
         dbRegister.collection("users").document(email).update(
-            "genre", "")
+            "aditional_info", info)
         dbRegister.collection("users").document(email).update(
-            "aditional_info", "")
-        dbRegister.collection("users").document(email).update(
-            "family", "")
+            "family", familyName)
 
         goNext()
     }
