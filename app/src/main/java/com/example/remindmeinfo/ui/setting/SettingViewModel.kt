b03d8341d842806920782fe160c724ac2a68ee18
AndyNuzzi <andy.nuzzi@gmail.com>
2023-11-24 13:48:05 +0100

    preparing setting archives

--- app/src/main/java/com/example/remindmeinfo/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/setting/SettingViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class SettingViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is setting test Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
