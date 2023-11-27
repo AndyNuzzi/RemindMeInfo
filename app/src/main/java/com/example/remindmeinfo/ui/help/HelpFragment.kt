1ca864eb9ad7a326a09b4336460a890cf3e72e53
AndyNuzzi <andy.nuzzi@gmail.com>
2023-11-27 13:06:09 +0100

    functionality of help interface

--- app/src/main/java/com/example/remindmeinfo/ui/help/HelpFragment.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/help/HelpFragment.kt
@@ -5,10 +5,20 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.TextView
+import com.example.remindmeinfo.R
 import com.example.remindmeinfo.databinding.FragmentHelpBinding
 
 class HelpFragment : Fragment() {
     private var _binding: FragmentHelpBinding? = null
+
+    private lateinit var message1: TextView
+    private lateinit var message2: TextView
+    private lateinit var message3: TextView
+    private lateinit var message4: TextView
+    private lateinit var message5: TextView
+    private lateinit var message6: TextView
+    private lateinit var message7: TextView
+    private lateinit var message8: TextView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -19,12 +29,18 @@ class HelpFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHelpBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = inflater.inflate(R.layout.fragment_help, container, false)
 
-        val textView: TextView = binding.message
+        message1 = view.findViewById(R.id.message1)
+        message2 = view.findViewById(R.id.message2)
+        message3 = view.findViewById(R.id.message3)
+        message4 = view.findViewById(R.id.message4)
+        message5 = view.findViewById(R.id.message5)
+        message6 = view.findViewById(R.id.message6)
+        message7 = view.findViewById(R.id.message7)
+        message8 = view.findViewById(R.id.message8)
 
-        return root
+        return view
     }
 
     override fun onDestroyView() {
