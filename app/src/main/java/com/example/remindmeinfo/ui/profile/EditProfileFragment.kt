6c8275d2f92cbd98daff8935eea183e581ad5d4f
AndyNuzzi <andy.nuzzi@gmail.com>
2024-03-25 12:59:38 +0100

    conecting profile editing fragment with bbdd

--- app/src/main/java/com/example/remindmeinfo/ui/profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/remindmeinfo/ui/profile/EditProfileFragment.kt
@@ -5,20 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.FragmentTransaction
 import com.example.remindmeinfo.R
 import com.example.remindmeinfo.databinding.FragmentEditProfileBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class EditProfileFragment : Fragment() {
 
     private var _binding: FragmentEditProfileBinding? = null
 
-    private lateinit var message1: TextView
+    val currentUser = FirebaseAuth.getInstance().currentUser
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,9 +31,43 @@ class EditProfileFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_edit_profile, container, false)
 
-        message1 = view.findViewById(R.id.text_edit_profile)
+        var email = currentUser?.email.toString()
+        var dbRegister = FirebaseFirestore.getInstance()
+
+        var name = view.findViewById<EditText>(R.id.textNameP).getText().toString()
+        var surname = view.findViewById<EditText>(R.id.textSurnameP).getText().toString()
+        var age = view.findViewById<EditText>(R.id.textAgeP).getText().toString()
+        var genre = view.findViewById<EditText>(R.id.textGenreP).getText().toString()
+        var info = view.findViewById<EditText>(R.id.textMedicalInfoP).getText().toString()
+        var familyName = view.findViewById<EditText>(R.id.familyNameP).getText().toString()
+
+
+        dbRegister.collection("users").document(email).update(
+            "name", name)
+        dbRegister.collection("users").document(email).update(
+            "surname", surname)
+        dbRegister.collection("users").document(email).update(
+            "age", age)
+        dbRegister.collection("users").document(email).update(
+            "genre", genre)
+        dbRegister.collection("users").document(email).update(
+            "aditional_info", info)
+        dbRegister.collection("users").document(email).update(
+            "family", familyName)
+
+        val myButton: Button = view.findViewById(R.id.buttonSaveEdit)
+        myButton.setOnClickListener {
+            // Aquí manejas el evento clic del botón
+            saveEdit()
+        }
 
         return view
+    }
+
+    private fun saveEdit() {
+        val fragmentTransaction: FragmentTransaction = requireFragmentManager().beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, ProfileAdminFragment())
+        fragmentTransaction.commit()
     }
 
 }
